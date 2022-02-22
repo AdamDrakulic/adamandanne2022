6ba6a758da88e717438ab1336c12ae3a2cf596df
AdamDrakulic <adrakuli@asu.edu>
2022-02-22 17:42:13 -0600

    added RSVP section

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // nothing to see here!
+
+// // Selecting the iframe element
+// var frame = document.getElementById("iframe");
+
+// // Adjusting the iframe height onload event
+// frame.onclick = function()
+// // function execute while load the iframe
+// {
+// // set the height of the iframe as 
+// // the height of the iframe content
+// frame.style.height = 
+// frame.contentWindow.document.body.scrollHeight + 'px';
+
+
+// // set the width of the iframe as the 
+// // width of the iframe content
+// frame.style.width  = 
+// frame.contentWindow.document.body.scrollWidth+'px';
+    
+// }
